a18a71572012a47c4c9f1152e588f2d2b14ed346
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-17 07:52:07 +0900

    W

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :agreements
   resources :orderers
+    Wice.define_routes(self, 'queries')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
