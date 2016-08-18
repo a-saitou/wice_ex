b1a0432bead3bfde877b6b42eb2b222d0f3ac1d2
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-19 07:26:58 +0900

    #

--- db/migrate/20160816221611_create_agreements.rb
+++ db/migrate/20160816221611_create_agreements.rb
@@ -1,9 +1,12 @@
 class CreateAgreements < ActiveRecord::Migration
   def change
     create_table :agreements do |t|
-      t.string :name
-      t.integer :orderer_id
-
+      t.string       :name
+      t.string       :abbreviation
+      t.integer      :code
+      t.integer     :orderer_id
+      t.time         :constraction_date
+      t.time         :completion_date
       t.timestamps null: false
     end
   end
