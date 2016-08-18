b4ed3d1dfc3e2f34ed6ec23bb8c90119d642d0d4
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-19 07:24:57 +0900

    #

--- db/migrate/20160816221530_create_orderers.rb
+++ db/migrate/20160816221530_create_orderers.rb
@@ -2,7 +2,7 @@ class CreateOrderers < ActiveRecord::Migration
   def change
     create_table :orderers do |t|
       t.string :name
-
+      t.string       :yomi
       t.timestamps null: false
     end
   end
