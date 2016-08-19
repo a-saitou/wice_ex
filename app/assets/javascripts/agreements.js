32b8389a294e816c9d1624a76e996b385894286a
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-19 22:12:50 +0900

    assets js

--- app/assets/javascripts/agreements.js
+++ app/assets/javascripts/agreements.js
@@ -1,12 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-  $(document).ready(function(){
-  $('#orderer_id').change(function(){
-    $.ajax({
-      url:"greements/find_by_orderer_id",
-      type: "GET",
-      data: { id: $('#orderer_id').val() }
-    });
-  });
-  })
