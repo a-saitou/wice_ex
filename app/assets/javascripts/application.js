f45136f1bfaa5f95c08d5cc99772aee7e86fdab1
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-20 12:08:05 +0900

    #

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-ja
 //= require bootstrap
 //= require wice_grid
